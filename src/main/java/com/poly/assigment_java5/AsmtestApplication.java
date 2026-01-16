63ffb948ac5b120cbdf39b40dce03ad3a001a0f8
Tonfa137206 <lequoctoan137@gmail.com>
2026-01-16 15:11:30 +0700

    khởi tạo dự án

---
+++ src/main/java/com/poly/assigment_java5/AsmtestApplication.java
@@ -0,0 +1,13 @@
+package com.poly.assigment_java5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/assigment_java5/AsmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.assigment_java5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
